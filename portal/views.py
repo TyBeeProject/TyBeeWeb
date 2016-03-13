e01ddca65ee63d442412d79284bbf8c2f094d7d6
Jacquin Théo <42nimag@gmail.com>
2016-03-13 01:04:25 +0100

    Add a basic views for homepage
    Defining a base.html in TyBeeWeb templates dirs

--- portal/views.py
+++ portal/views.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 
-# Create your views here.
+
+def home (request):
+    return render(request, 'portal/index.html')
