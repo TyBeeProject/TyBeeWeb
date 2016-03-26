a11f84837e7388ec469fc8ffc98a80356d22b529
Jacquin Théo <42nimag@gmail.com>
2016-03-26 09:05:44 +0100

    Graph monitoring feature implemented :
    Work with flot js library to render graph and navigate through it.
    
    * page /datas/hive/<id> show a graph for each captor of the Hive <id>.
    * added jquery
    * added flot needed files, so as the submodules
    * added some img of arrows for clickable navigation through the graph
    * upated requirements file

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^hive/(?P<idHive>\w+)$', views.seeHive, name='seeHive'),
     url(r'^putDatas', views.putDatas, name='putDatas'),
 ]
--- monitoring/views.py
+++ monitoring/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 from .models import Hive, Captor, Data
+
 import json
+
 
 # Affichage capteur, choix chronologie etc
 def putDatas(request):
@@ -28,3 +31,32 @@ def putDatas(request):
 
         return render(request, 'monitoring/putDatas.html')
     return HttpResponse (status = 401)
+
+
+def seeHive(request, idHive = 0):
+    try:
+        hive = Hive.objects.get(id = int(idHive))
+    except Hive.DoesNotExist:
+        # todo : raise a better 404
+        return HttpResponse(status = 404)
+    
+    # For each captors :
+    charts = []
+    for captor in hive.captor_set.all():
+        ident = captor.id
+        name = captor.name
+        datas = [[[data.timeStamp,data.value] for data in captor.data_set.all()]]
+        
+        yaxis = '{max: 100, min: 0, zoomRange: [0.1, 100], panRange : [0,100]}'
+        xaxis = ' {zoomRange: [0.1, 100], panRange: [0, '+ str(max([d[0] for d in datas]))+'] }'
+        opts = '{ yaxis :'+yaxis +', xaxis :'+ xaxis+', zoom : {interactive : true}, pan : { interactive : true} }'
+
+        chart = {'datas': str(datas), 'id':ident, 'options': opts, 'name': name}
+        
+        charts.append(chart)
+        
+    # chart = {'datas':str(charts), 'options':'{ yaxis : {max: 100}, xaxis: { zoomRange: [0.1, 100], panRange: [0, '+max([str(chart[0]) for chart in charts[0]])+'] }, yaxis: { zoomRange: [0.1, 100], panRange: [0, 100] }, zoom: { interactive: true },	pan: { interactive: true }}', 'id': 'A'}
+        
+    context = {'charts': charts}#, 'debug':debug}
+    return render(request, 'monitoring/seeHive.html', context)
+
