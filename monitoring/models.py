e4aa38f8e92461cb032f25668fabbd4eb787de10
Jacquin Théo <42nimag@gmail.com>
2016-03-27 18:36:19 +0200

    Refactoring model
    * Add formatDatas to captor model instead of hardcoding it in views
    * Starting the view of seeCaptor

--- monitoring/models.py
+++ monitoring/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+import time
+
 class Hive (models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
@@ -21,7 +23,35 @@ class Captor (models.Model):
         newData = Data(captor = self, timeStamp = ts, value = val)
         newData.save()        
         self.save()
+
         
+    def formatDatas(self, nbHoursFocus, nbHoursTotal):
+        # We multiply timestamp by 1000 because unix's one count seconds whereas js' one count miliseconds.        
+        datas = [[data.timeStamp*1000,data.value] for data in self.data_set.all()]
+        maxX = max([d[0] for d in datas])
+        maxY = max([d[1] for d in datas])
+        initialMaxX = time.time()*1000
+        initialMinX = initialMaxX - 1000*3600*nbHoursFocus
+        minX = max(initialMaxX - 1000*3600*nbHoursTotal,0)
+
+        # Determine the index to slice datas' values.
+        for index in range(len(datas)-1,0,-1):
+            if datas[index][0] < minX:                
+                break
+
+        formattedDatas = [datas[index:]]
+        
+        zoomMinX = 5
+        
+        yaxis = '{max: 100, min: 0, zoomRange: [100, 100], panRange : [0,100]}'
+        xaxis = r'{mode: "time", timeformat: "%d/%m - %H:%M:%S", max: '+ str(initialMaxX) +', min: '+str(initialMinX) +', zoomRange: ['+ str(zoomMinX) +', null], panRange: ['+ str(minX) +', '+ str(maxX) +'] }'
+        opts = '{ yaxis :'+yaxis +', xaxis :'+ xaxis+', zoom : {interactive : true}, pan : { interactive : true} }'
+
+        chart = {'datas': str(formattedDatas), 'id':self.id, 'options': opts, 'name': self.name}
+
+        return chart
+
+    
     def __str__ (self):
         return self.name + " (" + str(self.id)+ ")"
 
--- monitoring/views.py
+++ monitoring/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from .models import Hive, Captor, Data
 
 import json
-
+import time
 
 # Affichage capteur, choix chronologie etc
 def putDatas(request):
@@ -33,30 +33,37 @@ def putDatas(request):
     return HttpResponse (status = 401)
 
 
-def seeHive(request, idHive = 0):
+def seeHives(request):
+    hives = Hive.objects.all()
+    context = {'hives': hives}
+    return render(request, 'monitoring/seeHives.html', context)
+
+    
+def seeHive(request, idHive = 1):
     try:
         hive = Hive.objects.get(id = int(idHive))
+        captors = hive.captor_set.all()
     except Hive.DoesNotExist:
         # todo : raise a better 404
         return HttpResponse(status = 404)
     
     # For each captors :
     charts = []
-    for captor in hive.captor_set.all():
-        ident = captor.id
-        name = captor.name
-        datas = [[[data.timeStamp,data.value] for data in captor.data_set.all()]]
-        
-        yaxis = '{max: 100, min: 0, zoomRange: [0.1, 100], panRange : [0,100]}'
-        xaxis = ' {zoomRange: [0.1, 100], panRange: [0, '+ str(max([d[0] for d in datas]))+'] }'
-        opts = '{ yaxis :'+yaxis +', xaxis :'+ xaxis+', zoom : {interactive : true}, pan : { interactive : true} }'
-
-        chart = {'datas': str(datas), 'id':ident, 'options': opts, 'name': name}
-        
-        charts.append(chart)
-        
-    # chart = {'datas':str(charts), 'options':'{ yaxis : {max: 100}, xaxis: { zoomRange: [0.1, 100], panRange: [0, '+max([str(chart[0]) for chart in charts[0]])+'] }, yaxis: { zoomRange: [0.1, 100], panRange: [0, 100] }, zoom: { interactive: true },	pan: { interactive: true }}', 'id': 'A'}
-        
-    context = {'charts': charts}#, 'debug':debug}
+    for captor in captors:        
+        charts.append(captor.formatDatas(2, 24))
+                
+    context = {'hive': hive, 'captors': captors, 'charts': charts}
     return render(request, 'monitoring/seeHive.html', context)
 
+
+def seeCaptor(request, idHive = 1, idCaptor = 1):
+    try:
+        hive = Hive.objects.get(id = int(idHive))
+        captor = hive.captor_set.get(id = int(idCaptor))        
+    except Hive.DoesNotExist:
+        # todo : raise a better 404
+        return HttpResponse(status = 404)
+
+    chart = captor.formatDatas(24, 24*10)
+    context = {'hive': hive, 'captors': captors, 'chart': chart}
+    return render(request, 'monitoring/seeCaptor.html', context)
